2b847655160f3b12cf246d62f0b9893a2223372f
venkateshsakiri <venkateshsakiri377@gmail.com>
2024-12-15 12:13:17 +0530

    build error fixed

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AppComponent} from './app.component';
 import { MenuService } from './app.menu.service';
+import { RootScopeData } from './rootScope/rootScopeData';
 
 @Component({
     selector: 'app-menu',
@@ -42,28 +43,28 @@ export class AppMenuComponent implements OnInit {
             // {
             //     label: 'UI Kit', icon: 'pi pi-fw pi-star-fill', routerLink: ['/uikit'],
             //     items: [
-            //         {label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout']},
-            //         {label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input']},
-            //         {label: 'Float Label', icon: 'pi pi-fw pi-bookmark', routerLink: ['/uikit/floatlabel']},
-            //         {label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate']},
-            //         {label: 'Button', icon: 'pi pi-fw pi-mobile', routerLink: ['/uikit/button'], class: 'rotated-icon'},
-            //         {label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table']},
-            //         {label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list']},
-            //         {label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree']},
-            //         {label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel']},
-            //         {label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay']},
-            //         {label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media']},
+            //         {label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/dashboard/uikit/formlayout']},
+            //         {label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/dashboard/uikit/input']},
+            //         {label: 'Float Label', icon: 'pi pi-fw pi-bookmark', routerLink: ['/dashboard/uikit/floatlabel']},
+            //         {label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/dashboard/uikit/invalidstate']},
+            //         {label: 'Button', icon: 'pi pi-fw pi-mobile', routerLink: ['/dashboard/uikit/button'], class: 'rotated-icon'},
+            //         {label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/dashboard/uikit/table']},
+            //         {label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/dashboard/uikit/list']},
+            //         {label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/dashboard/uikit/tree']},
+            //         {label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/dashboard/uikit/panel']},
+            //         {label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/dashboard/uikit/overlay']},
+            //         {label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/dashboard/uikit/media']},
             //         {label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], preventExact: true},
-            //         {label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message']},
-            //         {label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file']},
-            //         {label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts']},
-            //         {label: 'Misc', icon: 'pi pi-fw pi-circle-off', routerLink: ['/uikit/misc']}
+            //         {label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/dashboard/uikit/message']},
+            //         {label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/dashboard/uikit/file']},
+            //         {label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/dashboard/uikit/charts']},
+            //         {label: 'Misc', icon: 'pi pi-fw pi-circle-off', routerLink: ['/dashboard/uikit/misc']}
             //     ]
             // },
             // {
             //     label:'Prime Blocks', icon:'pi pi-fw pi-prime', routerLink: ['/blocks'],
             //     items:[
-            //         {label: 'Free Blocks', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks']},
+            //         {label: 'Free Blocks', icon: 'pi pi-fw pi-eye', routerLink: ['/dashboard/blocks']},
             //         {label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: ['https://www.primefaces.org/primeblocks-ng'], target: '_blank'},
             //     ]
             // },
@@ -148,9 +149,15 @@ export class AppMenuComponent implements OnInit {
 
     public getAllEntitlements(){
         this.isLoadingComplete = true;
-        this.menuService.getEntitlements().subscribe((res:any)=>{
+        let user = RootScopeData.userInfo;
+        this.menuService.getEntitlements({role:user.user.UserRole}).subscribe((res:any)=>{
             this.isLoadingComplete = false;
             res.data.forEach((element:any) => {
+                if(element.key === 'ENTITLEMENTS'){
+                    this.model[1].items.push({
+                        label: element.name, icon: 'pi pi-fw pi-users', routerLink: ['/dashboard/entitlements']
+                    })
+                }
                 if(element.key === 'CUSTOMERS'){
                     this.model[1].items.push({
                         label:element.name,
@@ -160,7 +167,7 @@ export class AppMenuComponent implements OnInit {
                 }
                 if(element.key === 'LEADS'){
                     this.model[1].items.push({
-                        label: element.name, icon: 'pi pi-fw pi-dollar', routerLink: ['/dashboard/lead']
+                        label: element.name, icon: 'pi pi-fw pi-dollar', routerLink: ['/dashboard/leads']
                     })
                 }
                 if(element.key === 'QUOTES'){
